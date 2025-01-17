ea2ebecc44d4072d356203b6e7e6ec156a3dda88
Brendan J Crowe <brendancrowe98@gmail.com>
2025-01-17 16:38:40 -0700

    Add type annoations to models.py

--- set_transformer/models.py
+++ set_transformer/models.py
@@ -1,44 +1,65 @@
-from modules import *
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+from .modules import ISAB, PMA, SAB
+
 
 class DeepSet(nn.Module):
-    def __init__(self, dim_input, num_outputs, dim_output, dim_hidden=128):
+    def __init__(
+        self, dim_input: int, num_outputs: int, dim_output: int, dim_hidden: int = 128
+    ) -> None:
         super(DeepSet, self).__init__()
         self.num_outputs = num_outputs
         self.dim_output = dim_output
         self.enc = nn.Sequential(
-                nn.Linear(dim_input, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, dim_hidden))
+            nn.Linear(dim_input, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+        )
         self.dec = nn.Sequential(
-                nn.Linear(dim_hidden, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, dim_hidden),
-                nn.ReLU(),
-                nn.Linear(dim_hidden, num_outputs*dim_output))
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, dim_hidden),
+            nn.ReLU(),
+            nn.Linear(dim_hidden, num_outputs * dim_output),
+        )
 
-    def forward(self, X):
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
         X = self.enc(X).mean(-2)
         X = self.dec(X).reshape(-1, self.num_outputs, self.dim_output)
         return X
 
+
 class SetTransformer(nn.Module):
-    def __init__(self, dim_input, num_outputs, dim_output,
-            num_inds=32, dim_hidden=128, num_heads=4, ln=False):
+    def __init__(
+        self,
+        dim_input: int,
+        num_outputs: int,
+        dim_output: int,
+        num_inds: int = 32,
+        dim_hidden: int = 128,
+        num_heads: int = 4,
+        ln: bool = False,
+    ) -> None:
         super(SetTransformer, self).__init__()
         self.enc = nn.Sequential(
-                ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
-                ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln))
+            ISAB(dim_input, dim_hidden, num_heads, num_inds, ln=ln),
+            ISAB(dim_hidden, dim_hidden, num_heads, num_inds, ln=ln),
+        )
         self.dec = nn.Sequential(
-                PMA(dim_hidden, num_heads, num_outputs, ln=ln),
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
-                SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
-                nn.Linear(dim_hidden, dim_output))
+            PMA(dim_hidden, num_heads, num_outputs, ln=ln),
+            SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+            SAB(dim_hidden, dim_hidden, num_heads, ln=ln),
+            nn.Linear(dim_hidden, dim_output),
+        )
 
-    def forward(self, X):
+    def forward(self, X: torch.Tensor) -> torch.Tensor:
         return self.dec(self.enc(X))
